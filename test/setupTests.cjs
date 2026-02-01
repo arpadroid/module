2f732b3c87f8e09a45e12c838f3264ae484102ef
Andres Vaquero <web.andresvaquero@gmail.com>
2026-02-01 11:54:08 +1100

    phase 1 remediation

--- test/setupTests.cjs
+++ test/setupTests.cjs
@@ -1,12 +1,23 @@
-if (typeof jest !== 'undefined') {
+// Check if jest is already injected globally by Jest
+if (typeof jest === 'undefined') {
+    // Jest should inject globals when injectGlobals: true
+    // If jest is still undefined, we need to manually inject it
+    try {
+        if (typeof require !== 'undefined') {
+            const { jest: jestGlobal } = require('@jest/globals');
+            globalThis.jest = jestGlobal;
+            if (typeof global !== 'undefined') {
+                global.jest = jestGlobal;
+            }
+        }
+    } catch (err) {
+        // If require is not available, Jest globals should already be injected
+        console.warn('Could not manually inject jest globals:', err.message);
+    }
+} else {
+    // Ensure jest is available on globalThis and global
     globalThis.jest = jest;
     if (typeof global !== 'undefined') {
         global.jest = jest;
-    }
-} else {
-    const { jest: jestGlobal } = require('@jest/globals');
-    globalThis.jest = jestGlobal;
-    if (typeof global !== 'undefined') {
-        global.jest = jestGlobal;
     }
 }
