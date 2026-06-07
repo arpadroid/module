a2023cb60e50e37a42c5e42e8effa4569367b56c
Andres Vaquero <web.andresvaquero@gmail.com>
2026-06-07 12:24:16 +1000

    fixing class regex rule for stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,7 +5,7 @@ export default {
     plugins: [stylelintOrder],
     extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
     rules: {
-        'selector-class-pattern': '^[a-z][a-zA-Z0-9]*(?:__[a-zA-Z0-9]+)?(?:--[a-zA-Z0-9]+)?$',
+        'selector-class-pattern': '^[a-z][a-zA-Z0-9]*(?:__[a-zA-Z0-9]+)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?$',
         'keyframes-name-pattern': '^[a-z][a-zA-Z0-9]*$',
         'custom-property-pattern': '^[a-z][a-zA-Z0-9]*(?:-[a-zA-Z0-9]+)*$'
     }
