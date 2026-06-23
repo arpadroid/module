71db958d9256f461e5397f9373795e87a08014db
Andres Vaquero <web.andresvaquero@gmail.com>
2026-06-23 22:33:34 +1000

    fix class name regex

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,7 +5,8 @@ export default {
     plugins: [stylelintOrder],
     extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
     rules: {
-        'selector-class-pattern': '^[a-z][a-zA-Z0-9]*(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?$',
+        'selector-class-pattern':
+            '^[a-z][a-zA-Z0-9]*(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:[-_][a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:[-_][a-zA-Z0-9]+)*)?$',
         'keyframes-name-pattern': '^[a-z][a-zA-Z0-9]*$',
         'custom-property-pattern': '^[a-z][a-zA-Z0-9]*(?:-[a-zA-Z0-9]+)*$'
     }
